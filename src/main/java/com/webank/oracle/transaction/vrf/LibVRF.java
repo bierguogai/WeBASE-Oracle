d4c9a1f401c38278d75fd4ebc4c2982bb43210a9
yanyanho <yanyanho@126.com>
2020-10-29 11:29:33 +0800

    add ignore

---
+++ src/main/java/com/webank/oracle/transaction/vrf/LibVRF.java
@@ -0,0 +1,11 @@
+
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import org.springframework.core.io.ClassPathResource;
+
+public interface LibVRF extends Library {
+    LibVRF INSTANCE = (LibVRF) Native.loadLibrary(new ClassPathResource("./libvrf.dylib").getPath(), LibVRF.class);
+
+    String VRFProoFGenerate(String sk,String preseed);
+}
