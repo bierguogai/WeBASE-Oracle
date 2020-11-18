da32045f9d4bf9a3316711915ff755738f8f692c
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-11-18 16:56:27 +0800

    fix localdateformat

--- src/main/java/com/webank/oracle/Application.java
+++ src/main/java/com/webank/oracle/Application.java
@@ -2,7 +2,6 @@ package com.webank.oracle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
